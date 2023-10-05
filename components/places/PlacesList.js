4857bc1c03652364f7bf2c5a623df087b3dcffef
muticansel <cancel.muti@gmail.com>
2023-10-05 11:11:23 +0300

    feat: add PlaceItem and PlacesList components

--- components/places/PlacesList.js
+++ components/places/PlacesList.js
@@ -1,13 +1,35 @@
-import { FlatList } from "react-native";
+import { FlatList, View, Text, StyleSheet } from "react-native";
+import PlaceItem from "./PlaceItem";
 
 function PlacesList({ places }) {
+  if (!places || !places.length) {
+    return (
+      <View style={styles.fallbackContainer}>
+        <Text style={styles.fallbackText}>
+          No places added yet - start adding some!
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <FlatList
       data={places}
       keyExtractor={(item) => item.id}
-      renderItem={() => <div>Deneme</div>}
+      renderItem={({ item }) => <PlaceItem place={item} />}
     />
   );
 }
 
 export default PlacesList;
+
+const styles = StyleSheet.create({
+  fallbackContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  fallbackText: {
+    fontSize: 16,
+  },
+});
