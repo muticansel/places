ccf62b8316a480c9c2a262cbb675570cad94ec85
muticansel <cancel.muti@gmail.com>
2023-10-23 14:58:48 +0300

    feat: add react-navigation and new components

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import AllPlaces from "./screens/AllPlaces";
+import AddPlace from "./screens/AddPlace";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="All Places" component={AllPlaces} />
+          <Stack.Screen name="Add Place" component={AddPlace} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/places/PlaceForm.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const PlaceForm = () => {
+  return (
+    <View>
+      <Text>Place FORM</Text>
+    </View>
+  );
+};
+
+export default PlaceForm;
