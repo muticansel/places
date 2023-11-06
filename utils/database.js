a9b16129aa8c0727a3d1e9c2efeaa1dd4aa32305
muticansel <cancel.muti@gmail.com>
2023-11-06 11:15:46 +0300

    feat: add INSERT SQL for places

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,10 @@
-import * as SQLite from 'expo-sqlite';
+import * as SQLite from "expo-sqlite";
 
-const database = SQLite.openDatabase('places.db');
+const database = SQLite.openDatabase("places.db");
 
 export function init() {
   const promise = new Promise((resolve, reject) => {
-    database.transaction(tx => {
+    database.transaction((tx) => {
       tx.executeSql(
         `CREATE TABLE IF NOT EXISTS places (
         id INTEGER PRIMARY KEY NOT NULL, 
@@ -13,11 +13,33 @@ export function init() {
         address TEXT NOT NULL,
         lat REAL NOT NULL,
         lng REAL NOT NULL);`,
-        [], () => {
+        [],
+        () => {
           resolve();
-        }, (_, err) => {
+        },
+        (_, err) => {
           reject(err);
-        });
+        }
+      );
+    });
+  });
+
+  return promise;
+}
+
+export function insertPlace({ title, imageUri, address, lat, lng }) {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        `INSERT INTO places (title, imageUri, address, lat, lng) VALUES (?, ?, ?, ?, ?);`,
+        [title, imageUri, address, lat, lng],
+        (_, result) => {
+          resolve(result);
+        },
+        (_, err) => {
+          reject(err);
+        }
+      );
     });
   });
 
